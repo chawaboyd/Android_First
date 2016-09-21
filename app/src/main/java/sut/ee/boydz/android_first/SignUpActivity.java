ccc4dd57d31a741bda2edb3b3cdceaebfed20591
Boyd <chawaboyd@gmail.com>
2016-09-21 10:56:38 +0700

    strart 21 sep   - Bind widget

--- app/src/main/java/sut/ee/boydz/android_first/SignUpActivity.java
+++ app/src/main/java/sut/ee/boydz/android_first/SignUpActivity.java
@@ -2,13 +2,26 @@ package sut.ee.boydz.android_first;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, addrEditText, phoneEditText, userEditText, passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+    //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        addrEditText = (EditText)findViewById(R.id.editText2);
+        phoneEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
     } // Main Method
 
 
